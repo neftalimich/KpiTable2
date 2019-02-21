8dfe512548b6c228161e7006d308d109ab836d54
MSosaM <43592086+MSosaM@users.noreply.github.com>
2019-02-21 13:09:34 -0500

    Test
    Test

--- KpiTable2.js
+++ KpiTable2.js
@@ -314,13 +314,13 @@
                     }
                 });
 
-                $scope.backendApi.applyPatches([
-                    {
-                        "qPath": "/cube2/qHyperCubeDef/qDimensions",
-                        "qOp": "replace",
-                        "qValue": JSON.stringify(qDimensions)
-                    }
-                ], false);
+                // $scope.backendApi.applyPatches([
+                //     {
+                //         "qPath": "/cube2/qHyperCubeDef/qDimensions",
+                //         "qOp": "replace",
+                //         "qValue": JSON.stringify(qDimensions)
+                //     }
+                // ], false);
             });
             $scope.$watchCollection("layout.cube2Measures", function (newVal) {
                 let qMeasures = [];
@@ -338,13 +338,13 @@
                     }
                 });
 
-                $scope.backendApi.applyPatches([
-                    {
-                        "qPath": "/cube2/qHyperCubeDef/qMeasures",
-                        "qOp": "replace",
-                        "qValue": JSON.stringify(qMeasures)
-                    }
-                ], false);
+                // $scope.backendApi.applyPatches([
+                //     {
+                //         "qPath": "/cube2/qHyperCubeDef/qMeasures",
+                //         "qOp": "replace",
+                //         "qValue": JSON.stringify(qMeasures)
+                //     }
+                // ], false);
             });
             $scope.$watchCollection("layout.cube2.qHyperCube.qDataPages", function (newVal) {
                 angular.element(document).ready(function () {
@@ -472,6 +472,7 @@
                     $(".chart2-" + group.name).each(function () {
                         Highcharts.chart({
                             chart: {
+                                type: "line",
                                 renderTo: this,
                                 backgroundColor: 'rgba(255, 255, 255, 0.0)'
                             },
